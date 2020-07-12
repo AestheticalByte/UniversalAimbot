054824a6a922114537fa5298fa22283d72779536
AestheticalByte <atkosmin@gmail.com>
2020-07-12 14:05:33 -0600

    Update Main.lua

--- Universal/Main.lua
+++ Universal/Main.lua
@@ -16,7 +16,6 @@ local fovEnabled = true;
 local fovSides = 50; 
 local aimbotToggled;
 local headToggled = true;
-local verNumber = '1.1.2';
 
 -- // Aimbot library
 local EzAimbot = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/AestheticalByte/UniversalAimbot/master/Universal/AimbotLibrary.lua"))();
@@ -47,4 +46,3 @@ UserInputService.InputBegan:Connect(function(key)
 end)
 
 warn('Aimbot script loaded...');
-warn('version. '..verNumber);
