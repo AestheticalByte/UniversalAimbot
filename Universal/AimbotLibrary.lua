96fb921f05e7aaa7f9062cbc547fb2b9ed878820
AestheticalByte <atkosmin@gmail.com>
2020-07-13 14:05:47 -0600

    Revert "Fixed healthCheck() not working."
    This reverts commit 2d6e234fbeef59b1c9c15f7e8930635f5c98a163.

--- Universal/AimbotLibrary.lua
+++ Universal/AimbotLibrary.lua
@@ -6,7 +6,7 @@
 local EzAimbot = {}
 
 --// Internal
-local buildID = "0.0.5"
+local buildID = "0.0.2"
 warn('Build ID : '..buildID)
 
 local aimPart;
@@ -62,7 +62,7 @@ function healthCheck(plr)
     elseif plr.Character.Humanoid.Health == 0 then
         return false;
     end
-    return false;
+    print(plr.Character.Humanoid.Health)
 end
 
 function checkHumanoid(tb)
@@ -114,7 +114,6 @@ local lockPlayer = function(friendlyfire)
         end)
     end
     if setTarget and playerSet then
-        setTarget = game.Players:FindFirstChild(setTarget.Name)
         if healthCheck(setTarget) then
 
             aimingPart(setTarget);
@@ -129,12 +128,7 @@ local lockPlayer = function(friendlyfire)
                 return setTarget;
             end
             warn("Issue encountered!")
-            playerSet = false;
-            setTarget = nil;
-            return setTarget;
-        elseif not healthCheck(setTarget) then
-            print("You done fucked up!")
-            playerSet = false;
+            playerSet = nil;
             setTarget = nil;
             return setTarget;
         end
@@ -186,7 +180,7 @@ EzAimbot.Enable = function(showfov,fovconfig, friendlyfire)
         FOV.Visible = true
     end
 
-    rate = 0.0025; --(runs loop regardless of frames)
+    rate = 0.003; --(runs loop regardless of frames)
     local amount = 0;
     MainLoop = RunService.Heartbeat:Connect(function(dlTime)
         amount = amount + dlTime;
